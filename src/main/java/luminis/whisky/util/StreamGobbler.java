b103e170ad4f7a9615515f483839f7b200504f26
Roger Janssen <rogerjanssen@Rogers-MacBook-Pro.local>
2015-11-01 22:05:14 +0100

    cleanup

--- src/main/java/luminis/whisky/util/StreamGobbler.java
+++ src/main/java/luminis/whisky/util/StreamGobbler.java
@@ -5,21 +5,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * Writes an input stream to system.out.
+ */
 public class StreamGobbler extends Thread {
-    private final InputStream is;
+    private final InputStream inputStream;
     private final String type;
 
-    public StreamGobbler(InputStream is, String type) {
-        this.is = is;
+    public StreamGobbler(InputStream inputStream, String type) {
+        this.inputStream = inputStream;
         this.type = type;
     }
 
     public void run() {
         try
         {
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String line=null;
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(reader);
+            String line;
             while ( (line = br.readLine()) != null)
                 System.out.println(type + ">" + line);
         } catch (IOException ioe) {
