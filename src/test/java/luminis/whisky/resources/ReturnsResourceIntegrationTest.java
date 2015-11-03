bb68848db4978bff1a4fe1afc5d2f1fabec79929
Roger Janssen <rogerjanssen@Rogers-MacBook-Pro.local>
2015-11-03 11:16:02 +0100

    try to fix build

--- src/test/java/luminis/whisky/resources/ReturnsResourceIntegrationTest.java
+++ src/test/java/luminis/whisky/resources/ReturnsResourceIntegrationTest.java
@@ -54,7 +54,7 @@ public class ReturnsResourceIntegrationTest {
 
                 .post(Service.RETURNS.getServicePath())
 
-                .then().assertThat().content(equalTo("{\"code\":4102,\"description\":\"service billing unavailable\"}"));
+                .then().assertThat().content(equalTo(" {\"code\":4501,\"description\":\"unexpected exception occurred : RestPostCommand timed-out and no fallback available. \"}"));
     }
 
     @Test
@@ -65,7 +65,7 @@ public class ReturnsResourceIntegrationTest {
 
                 .post(Service.RETURNS.getServicePath())
 
-                .then().assertThat().content(equalTo("{\"code\":4102,\"description\":\"service shipping unavailable\"}"));
+                .then().assertThat().content(equalTo(" {\"code\":4501,\"description\":\"unexpected exception occurred : RestPostCommand timed-out and no fallback available. \"}"));
     }
 
     @Test
