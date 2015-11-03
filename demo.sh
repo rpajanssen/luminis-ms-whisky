2b9b225f54d924626daee95a839a02726527cd3e
Roger Janssen <rogerjanssen@Rogers-MacBook-Pro.local>
2015-11-03 10:37:01 +0100

    demo prepwork

--- demo.sh
+++ demo.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package
-boxfuse fuse target/whiskyreturns-0.3.jar -jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -ports.tracing=5005
-boxfuse run whiskyreturns:0.3 -portsmap.tracing=5105 -envvars.CONSUL_SKIP_DEPLOYMENT=true -envvars.CONSUL_AGENT_ADDRESS=192.168.0.15 -envvars.WITHOUT_STUBS=true
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -DCONSUL_SKIP_DEPLOYMENT=true -DBOXFUSE_PORTS_HTTP=8080 -DBOXFUSE_PORTS_FORWARDED_HTTP=8080 -DWITHOUT_STUBS=true -DBOXFUSE_ENV=test -jar target/whiskyreturns-0.3.jar server target/classes/demo.yml
